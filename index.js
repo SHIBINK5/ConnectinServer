2e1831f6b35831b660dd0e0ef2444080afda3dba
Shibin k <shibink321@gmail.com>
2023-02-23 18:29:27 +0530

    socket implemented in server

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import {users,posts} from "./data/index.js";
 
+import {createServer} from 'http';
+import {Server} from 'socket.io';
+
+
 /* CONFIGURATIONS */
 
  const __filename = fileURLToPath(import.meta.url);
@@ -35,7 +39,10 @@ import {users,posts} from "./data/index.js";
 app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
-app.use(cors());
+app.use(cors({
+  origin:["https://connectins.netlify.app","http://localhost:3000"],
+  credentials:true
+}));
 app.use("/assets",express.static(path.join(__dirname,'public/assets')));
 import { editProfilePic } from './controllers/users.js';
 /*FILE STORAGE*/
@@ -61,6 +68,56 @@ app.use("/posts",postRoutes);
 app.use("/chat", chatRoutes)
 app.use("/message", MessageRoute)
 
+
+const httpServer=createServer(app);
+const io=new Server(httpServer,{
+  cors:{
+    origin:["https://connectins.netlify.app","http://localhost:3000"]
+  }
+})
+
+
+
+let activeUsers = [];
+
+
+io.on("connection", (socket) => {
+    console.log("ddddd",socket.id);   
+    // add new User
+    socket.on("new-user-add", (newUserId) => {
+        // if user is not added previously
+        console.log('wwww');
+        if (!activeUsers.some((user) => user.userId === newUserId)) {
+            activeUsers.push({ userId: newUserId, socketId: socket.id });
+            console.log(activeUsers);
+        }
+        console.log("New User Connected", activeUsers);
+        // send all active users to new user
+        io.emit("get-users", activeUsers);
+    });
+
+    socket.on("disconnect", () => {
+        // remove user from active users
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+        console.log("User Disconnected", activeUsers);
+        // send all active users to all users
+        io.emit("get-users", activeUsers);
+    });
+
+    // send message to a specific user
+    socket.on("send-message", (data) => {
+        const { receiverId } = data;
+        const user = activeUsers.find((user) => user.userId === receiverId);
+        console.log("Sending from socket to :", receiverId)
+        console.log("Data: ", data)
+        if (user) {
+            io.to(user.socketId).emit("recieve-message", data);
+        }
+    });
+})
+
+
+
 /* MONGOOSE SETUP*/
 const PORT=process.env.PORT || 6001;
 mongoose
@@ -70,7 +127,7 @@ mongoose
 
   })
 .then(() =>{
-  app.listen(PORT,() => console.log(`Server Port:${PORT}`));
+  httpServer.listen(PORT,() => console.log(`Server Port:${PORT}`));
  
   /* ADD DATA ONE TIME */
   // User.insertMany(users);
